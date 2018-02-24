c1ed41e025ea54775a73014b2b0da2554849ce12
Juan Ramirez <ep.juan@gmail.com>
2018-02-24 08:48:29 -0500

    Inicializacion de Resumen de Archvios

---
+++ ManejoRIPS/ManejoArchivos/CArchivos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManejoRIPS.ManejoArchivos
+{
+    class CArchivos
+    {
+        private string NumeroLineas;
+        private DirectoryInfo Directorio;
+        private string Nombre;
+
+        public string ContarLineas(string PathFile)
+        {
+            if (File.Exists(PathFile))
+            {
+                using (StreamReader reader = new StreamReader(PathFile, Encoding.GetEncoding("Windows-1252")))
+                {
+                    NumeroLineas = File.ReadAllLines(PathFile).Length.ToString();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Archivo no Existe");
+            }
+
+            return NumeroLineas;
+        }
+
+        /// <summary>
+        /// Retorna una Lista de todos los archivos de una carpeta según un criterio
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        /// <param name="searchPattern"></param>
+        /// <returns></returns>
+        public List<string> ListarArchivos(string directoryPath, string searchPattern)
+        {
+            Directorio = new DirectoryInfo(directoryPath);
+
+            List<string> files = new List<string>();
+
+            foreach (var file in Directorio.GetFiles(searchPattern, SearchOption.TopDirectoryOnly))
+            {
+
+                String nameFile = file.FullName;
+                files.Add(nameFile);
+
+            }
+
+            return files;
+        }
+
+
+        // Número de Lineas Segun Archivo
+        public string Lineas(string directoryPath, string searchPattern)
+        {
+            Directorio = new DirectoryInfo(directoryPath);
+
+            foreach (var item in Directorio.GetFiles(searchPattern))
+            {
+                String nameFile = item.FullName;
+                string NumeroLineas = ContarLineas(nameFile);
+            }
+
+            return NumeroLineas;
+        }
+
+        public string NombreArchivo(string directoryPath, string searchPattern)
+        {
+            Directorio = new DirectoryInfo(directoryPath);
+
+            foreach (var item in Directorio.GetFiles(searchPattern))
+            {
+                Nombre = item.Name;
+            }
+
+            return Nombre;
+        }
+    }
+}
--- ManejoRIPS/Page2.xaml.cs
+++ ManejoRIPS/Page2.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using ManejoRIPS.ManejoArchivos;
 
 namespace ManejoRIPS
 {
@@ -26,12 +27,47 @@ namespace ManejoRIPS
             InitializeComponent();
         }
 
+        string dirPath;
+        CArchivos oUS = new CArchivos();
+        CArchivos oAC = new CArchivos();
+        CArchivos oAM = new CArchivos();
+        CArchivos oAD = new CArchivos();
+        CArchivos oCT = new CArchivos();
+        CArchivos oAF = new CArchivos();
+        CArchivos oAP = new CArchivos();
+
         private void btnRuta_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog folder = new FolderBrowserDialog();
-            folder.ShowDialog();
 
-            txtFolder.Text = folder.SelectedPath;
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                dirPath = folder.SelectedPath;
+
+                txtFolder.Text = dirPath;
+
+                txtLineasUS.Text = oUS.Lineas(dirPath, "*US*");
+                txtNomUS.Text = oUS.NombreArchivo(dirPath, "*US*");
+
+                txtLineasAC.Text = oAC.Lineas(dirPath, "*AC*");
+                txtNomAC.Text = oAC.NombreArchivo(dirPath, "*AC*");
+
+                txtLineasAM.Text = oAM.Lineas(dirPath, "*AM*");
+                txtNomAM.Text = oAM.NombreArchivo(dirPath, "*AM*");
+
+                txtLineasAD.Text = oAD.Lineas(dirPath, "*AD*");
+                txtNomAD.Text = oAD.NombreArchivo(dirPath, "*AD*");
+
+                txtLineasCT.Text = oCT.Lineas(dirPath, "*CT*");
+                txtNomCT.Text = oCT.NombreArchivo(dirPath, "*CT*");
+
+                txtLineasAF.Text = oAF.Lineas(dirPath, "*AF*");
+                txtNomAF.Text = oAF.NombreArchivo(dirPath, "*AF*");
+
+                txtLineasAP.Text = oAP.Lineas(dirPath, "*AP*");
+                txtNomAP.Text = oAP.NombreArchivo(dirPath, "*AP*");
+            }
+
         }
     }
 }
